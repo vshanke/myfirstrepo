60c7b415cd7594c6a12fbe77a87384d49bd987fe
Vrushali Shanke <vrushali.shanke@gmail.com>
2025-06-22 17:51:55 +1000

    api test

--- test_function.py
+++ test_function.py
@@ -1,4 +1,5 @@
 from myfunc import add
+import requests
 
 def test_add():
     assert add(2,3) == 5
@@ -7,7 +8,20 @@ def test_add1():
     assert add(2,6) == 8
 
 def test_add2():
-    assert add(2,5) == 9
+    assert add(2,5) == 7
 
 def test_add3():
     assert add(2, 2) == 4
+
+# tests/test_jsonplaceholder.py
+
+def test_get_post_1():
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+    response = requests.get(url)
+    
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data["id"] == 1
+    assert "title" in data
+    assert "body" in data
